6f22eb611b5c9cd394ecac39274513e7d991522e
BangpengGao <bangpenggao@163.com>
2019-12-20 16:13:33 +0800

    Add files via upload

---
+++ RegularExpressionMatching.java
@@ -0,0 +1,22 @@
+package com.leetcode.learn;
+/*
+ * @ Created with IntelliJ IDEA
+ * @ Author Bangpeng Gao
+ * @ Version 1.0.0
+ * @ Since 1.0.0
+ * @ Date 2019/12/19
+ * @ Time 17:15
+ */
+
+import java.util.regex.Pattern;
+
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        return Pattern.matches(p, s);
+    }
+
+    public static void main(String[] args) {
+        RegularExpressionMatching r = new RegularExpressionMatching();
+        System.out.println(r.isMatch("aa", "a"));
+    }
+}
